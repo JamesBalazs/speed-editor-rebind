e542623b290b53d01c83d8cdfca2d5aaca2b3e88
James Balazs <j.c.balazs1@gmail.com>
2026-04-04 09:14:21 +1100

    wip toolbox with quasar components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -448,18 +448,30 @@ import Key from "./components/Key.vue";
             </div>
         </div>
     </div>
+
+    <q-card dark bordered flat class="toolbox">
+        <q-toolbar class="glossy row">
+            <q-toolbar-title>Choose Action</q-toolbar-title>
+        </q-toolbar>
+
+        <q-tabs align="left">
+            <q-tab name="button" label="Button"></q-tab>
+            <q-tab name="led" label="LED Behaviour"></q-tab>
+        </q-tabs>
+
+        <q-separator dark></q-separator>
+    </q-card>
 </template>
 
 <style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-}
-.logo:hover {
-    filter: drop-shadow(0 0 2em #e80000aa);
-}
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+.toolbox {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100vw;
+    height: 300px;
+    border-radius: 0;
+    z-index: 1000;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { Quasar } from "quasar";
+import "quasar/dist/quasar.css";
+import "@quasar/extras/material-icons/material-icons.css";
+
+const app = createApp(App);
+
+app.use(Quasar, {
+    plugins: {},
+});
+
+app.mount("#app");
